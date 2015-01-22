5095b348cc46ede5d74569975e8364ea74011bd0
Yohan Jarosz <yohanjarosz@yahoo.fr>
2015-01-22 13:52:19 +0100

    addd mirrors file

---
+++ manifests/mirrors.pp
@@ -0,0 +1,33 @@
+class apt::params ($distid, $distcodename, $mirror = undef){
+  $mirror = $mirror
+  if $mirror == undef {
+    case $distid {
+      'debian': {
+        case $distcodename {
+          'squeeze': {
+            $mirror = 'http://backports.debian.org'
+          }
+          'wheezy': {
+            $mirror = 'http://ftp.debian.org'
+          }
+          default: {
+            $mirror = 'http://http.debian.net'
+          }
+        }
+      }
+      'ubuntu': {
+        case $distcodename {
+          'lucid': {
+            $mirror = 'http://us.archive.ubuntu.com'
+          }
+          'precise', 'trusty', 'utopic': {
+            $mirror = 'http://us.archive.ubuntu.com'
+          }
+          default: {
+            $mirror = 'http://old-releases.ubuntu.com'
+          }
+        }
+      }
+    }
+  }
+}
